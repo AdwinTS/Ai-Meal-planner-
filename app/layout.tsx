0a74ba260005d568f5cd510181e17d0490d24af8
AdwinTS <adwintsunil8@gmail.com>
2025-06-27 13:59:30 +0530

    some changes done to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Ai Meal Planner',
+  description: 'Created for ai generted meal plans with ingredients and cooking instructions',
   generator: 'v0.dev',
+  icons: {
+    icon: "/favicon.ico",
+  },
+
 }
 
 export default function RootLayout({
